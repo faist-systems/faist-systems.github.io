a90143dde4908955f29aec786b186c2566bcfb81
Teddy-design-88 <hora.tadeas@gmail.com>
2026-03-05 19:33:15 +0100

    Create world.js

---
+++ play/games/dollhouse/world.js
@@ -0,0 +1,11 @@
+// ======================================
+// FAIST – World Constants
+// ======================================
+
+export const ROOM_WIDTH = 800;
+export const ROOM_HEIGHT = 360;
+
+export const FLOOR_DEPTH = 240;
+export const WALL_HEIGHT = ROOM_HEIGHT - FLOOR_DEPTH;
+
+export const AVATAR_SPEED = 4;
